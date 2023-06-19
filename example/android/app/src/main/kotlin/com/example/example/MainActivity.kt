932baa50b76a2cd5e4f9694d70688ce25af863a8
chetan-cueclad <chetan.gairwar@cueclad.com>
2023-06-19 12:25:45 +0530

    Fixed errors.

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.example
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+//import androidx.annotation.NonNull;
+//import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.engine.FlutterEngine
+//import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
 }
